9cc443e40d7b13b06e81710a285a1b747f6a699c
DarkCaster <dark.caster@outlook.com>
2016-09-04 12:16:49 +0300

    Added deploy-overlay.sh script + description

--- Deploy/README.md
+++ Deploy/README.md
@@ -7,3 +7,8 @@
 Script that perform backup of overlay-fs from running openwrt router - only changed files relative to read-only firmware-root are backed up.
 For use only with squash-fs + overlay-fs firmware configurations. You also need rsync to be installed on the router side.
 
+## deploy-overlay.sh
+
+Script that perform deploy of previously backed up overlay files, may be used to upload files to router at different target directory instead of "/" (root dir).
+For use only with squash-fs + overlay-fs firmware configurations. You also need rsync to be installed on the router side.
+
---
+++ Deploy/deploy-overlay.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+showusage () {
+ echo "Usage: deploy-config.sh <router ip> <config-dir> [target-dir-on-router]"
+}
+
+check_error () {
+local ec="$?"
+if [ "$ec" != "0" ]; then
+ echo "last command completed with error!"
+ exit $ec
+fi
+}
+
+ipaddr="$1"
+test "z$ipaddr" = "z" && showusage && exit 3
+
+cfgdir="$2"
+test "z$cfgdir" = "z" && showusage && exit 2
+
+if [ ! -d "$cfgdir" ]; then
+ script_dir="$( cd "$( dirname "$0" )" && pwd )"
+ cfgdir="$script_dir/$cfgdir"
+fi
+
+test ! -d "$cfgdir" && echo "$cfgdir directory is missing" && exit 1
+
+tgtdir="$3"
+
+rsync -vcrlHpEogDtW --chown=root:root --inplace "$cfgdir"/ root@$ipaddr:"$tgtdir"/
+check_error
+
