9039b4e6abf389cb3cf30535e32dd0045d47c60b
DarkCaster <dark.caster@outlook.com>
2018-02-02 00:17:09 +0300

    pre-step-3-uninstall-pkg-gcc6.sh script updated

--- Build/pre-step-3-uninstall-pkg-gcc6.sh
+++ Build/pre-step-3-uninstall-pkg-gcc6.sh
@@ -8,3 +8,4 @@
 ./scripts/feeds uninstall gcc #native toolchain for run on target platform, using gcc 5.4 - failing to build with gcc 6.x cross-toolchain
 ./scripts/feeds uninstall haveged #Error: operand value out of range for instruction
 ./scripts/feeds uninstall linknx  # error: 'suncalc::std::abs' has not been declared
+./scripts/feeds uninstall netatalk # ???
