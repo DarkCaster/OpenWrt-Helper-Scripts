4360e8063bf84e4582b675196e8e16cf435bbee3
DarkCaster <dark.caster@outlook.com>
2019-05-07 05:14:23 +0300

    pre-step-2-prepare-src-openwrt1806-modemmanager.sh script updated

--- Build/pre-step-2-prepare-src-openwrt1806-modemmanager.sh
+++ Build/pre-step-2-prepare-src-openwrt1806-modemmanager.sh
@@ -8,13 +8,14 @@ script_dir="$( cd "$( dirname "$0" )" && pwd )"
 # prepare custom feeds
 echo "src-git mobile_broadband https://gitlab.freedesktop.org/mobile-broadband/mobile-broadband-openwrt.git" > feeds.conf
 cat feeds.conf.default >> feeds.conf
-sed -i "s|src-git luci https://git.openwrt.org/project/luci.git;openwrt-18.06|src-link luci /tmp/luci-openwrt1806|g" feeds.conf
+sed -i "s|src-git luci https://git.openwrt.org/project/luci.git;openwrt-18.06|src-link luci ./luci-openwrt1806|g" feeds.conf
 
-rm -rf /tmp/luci-openwrt1806
-rm -rf /tmp/luci-proto-modemmanager
-git clone -b openwrt-18.06 https://git.openwrt.org/project/luci.git /tmp/luci-openwrt1806
-git clone https://bitbucket.org/mips171/luci-proto-modemmanager.git /tmp/luci-proto-modemmanager
-mv /tmp/luci-proto-modemmanager/luci-proto-modemmanager /tmp/luci-openwrt1806/protocols/
+rm -rf ./luci-openwrt1806
+rm -rf ./luci-proto-modemmanager
+git clone -b openwrt-18.06 https://git.openwrt.org/project/luci.git ./luci-openwrt1806
+git clone https://bitbucket.org/mips171/luci-proto-modemmanager.git ./luci-proto-modemmanager
+mv ./luci-proto-modemmanager/luci-proto-modemmanager ./luci-openwrt1806/protocols/
+rm -rf ./luci-proto-modemmanager
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
