45d1795a4f4c3b6d473800045b2068df63eb510f
DarkCaster <dark.caster@outlook.com>
2019-07-19 03:01:56 +0300

    Added preparation scripts for openwrt 19.07

---
+++ Build/pre-step-2-prepare-src-openwrt1907.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+
+set -e
+
+script_dir="$( cd "$( dirname "$0" )" && pwd )"
+
+# prepare custom feeds
+echo "src-git custom https://github.com/DarkCaster/OpenWrt-Custom-Feed.git" > feeds.conf
+cat feeds.conf.default >> feeds.conf
+
+./scripts/feeds update -a
+./scripts/feeds install -a
+
+test -d "$script_dir/dl" && rsync -vrlHpEAXogDtSW --numeric-ids --delete-before "$script_dir/dl" .
+
+rm -v .config .config.old
---
+++ Build/pre-step-4-uninstall-pkg-openwrt1907.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+#
+
+# TODO
+# additional packages to uninstall for openwrt 19.07, using gcc7
+
+# this packages removed to fix broken dependencies
+./scripts/feeds uninstall domoticz
+./scripts/feeds uninstall freeswitch-stable
+./scripts/feeds uninstall freeswitch-stable-mod-bcg729
+./scripts/feeds uninstall freifunk-gwcheck
+./scripts/feeds uninstall freifunk-mapupdate
+./scripts/feeds uninstall kamailio-5.x
+./scripts/feeds uninstall lcdringer
+./scripts/feeds uninstall ldbus
+./scripts/feeds uninstall luci-app-e2guardian
+./scripts/feeds uninstall luci-app-olsr
+./scripts/feeds uninstall luci-app-olsr-services
+./scripts/feeds uninstall luci-app-olsr-viz
+./scripts/feeds uninstall luci-mod-freifunk-community
+./scripts/feeds uninstall node-mozilla-iot-gateway
+./scripts/feeds uninstall prometheus-node-exporter-lua
+
+# seems incompatible with gcc 6
+./scripts/feeds uninstall addrwatch
+./scripts/feeds uninstall bonnie++
+./scripts/feeds uninstall clamsmtp
+./scripts/feeds uninstall cmdpad
+./scripts/feeds uninstall espeak # try to disable -Wnarrowing warning and retest
+./scripts/feeds uninstall jool
+./scripts/feeds uninstall kismet # needs uclibc
+./scripts/feeds uninstall lcd4linux # undefined reference to `drv_G15_evalScanCode'
+./scripts/feeds uninstall libartnet # -Werror=memset-elt-size
+./scripts/feeds uninstall libsoup # Unescaped left brace in regex is illegal here in regex
+./scripts/feeds uninstall mutt
+./scripts/feeds uninstall nail
+./scripts/feeds uninstall oath-toolkit
+./scripts/feeds uninstall psqlodbc
+./scripts/feeds uninstall quassel-irssi #-Werror=implicit-fallthrough=
+./scripts/feeds uninstall rxtx # needs java (javac) for build
+./scripts/feeds uninstall v4l2rtspserver # needs libasound.so.2
+./scripts/feeds uninstall zmq #fatal error: basic_definitions: No such file or directory
+./scripts/feeds uninstall pimbd
+# ./scripts/feeds uninstall omcproxy # cannot be uninstalled ? disabled in config, instead
+
+# does not build because of broken gstreamer1
+./scripts/feeds uninstall gstreamer1
+./scripts/feeds uninstall gnunet
+./scripts/feeds uninstall gst1-plugins-ugly
+./scripts/feeds uninstall gst1-plugins-good
+./scripts/feeds uninstall gst1-plugins-base
+./scripts/feeds uninstall gst1-plugins-bad
+./scripts/feeds uninstall gnunet-secushare
+./scripts/feeds uninstall gst1-libav
+./scripts/feeds uninstall libextractor
+./scripts/feeds uninstall libdmapsharing
+./scripts/feeds uninstall dmapd
+
+# other
+./scripts/feeds uninstall autossh # ssh command not found ?
+./scripts/feeds uninstall luajit # fatal error: bits/libc-header-start.h: No such file or directory
