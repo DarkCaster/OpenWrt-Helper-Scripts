06e35fca38c1675356a580bc4a52e25d339d6408
DarkCaster <dark.caster@outlook.com>
2019-11-03 02:50:20 +0300

    Build/OpenWrt-1907-custom/01-uninstall-pkg.sh script updated

--- Build/OpenWrt-1907-custom/01-uninstall-pkg.sh
+++ Build/OpenWrt-1907-custom/01-uninstall-pkg.sh
@@ -3,6 +3,7 @@
 
 # packages to uninstall for openwrt 19.07 in order to fix broken stuff
 
-./scripts/feeds uninstall nfs-kernel-server # menuconfig tmp/.config-package.in:55375:error: recursive dependency detected!
-./scripts/feeds uninstall samba4 # depends on nfs-kernel-server
-./scripts/feeds uninstall luci-app-samba4 # depends on samba4
+# usage: ./scripts/feeds uninstall <package name>
+
+# no broken packages currently
+exit 0
