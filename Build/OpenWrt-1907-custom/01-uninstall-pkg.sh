4fb203df260310bce2b23d57131290acc00b2859
DarkCaster <dark.caster@outlook.com>
2019-11-03 02:56:54 +0300

    Build/OpenWrt-1907-custom/01-uninstall-pkg.sh script updated

--- Build/OpenWrt-1907-custom/01-uninstall-pkg.sh
+++ Build/OpenWrt-1907-custom/01-uninstall-pkg.sh
@@ -5,5 +5,9 @@
 
 # usage: ./scripts/feeds uninstall <package name>
 
-# no broken packages currently
-exit 0
+./scripts/feeds uninstall nfs-kernel-server # menuconfig tmp/.config-package.in:55375:error: recursive dependency detected!
+./scripts/feeds uninstall samba4 # depends on nfs-kernel-server
+./scripts/feeds uninstall luci-app-samba4 # depends on samba4
+
+./scripts/feeds uninstall seafile-seahub # has a dependency on 'django-simple-captcha', which does not exist
+./scripts/feeds uninstall seafile-server # depends on seafile-seahub
