955c6a29d39d9434c9f0102b10095c6f7a9f2a08
DarkCaster <dark.caster@outlook.com>
2023-04-18 02:12:12 +0300

    Build/OpenWrt-2102-custom scripts updated

--- Build/OpenWrt-2102-custom/00-prepare-src.sh
+++ Build/OpenWrt-2102-custom/00-prepare-src.sh
@@ -6,8 +6,8 @@ set -e
 script_dir="$( cd "$( dirname "$0" )" && pwd )"
 
 # prepare custom feeds
-echo "src-git custom https://github.com/DarkCaster/OpenWrt-Custom-Feed.git" > feeds.conf
-cat feeds.conf.default >> feeds.conf
+cat feeds.conf.default > feeds.conf
+echo "src-git custom https://github.com/DarkCaster/OpenWrt-Custom-Feed.git" >> feeds.conf
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
