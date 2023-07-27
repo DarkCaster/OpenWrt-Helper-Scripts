374f75d07920c7658d9c3c2a8078f6131f6768b4
DarkCaster <dark.caster@outlook.com>
2023-07-27 16:42:30 +0300

    Build/OpenWrt-2305-custom scripts updated

--- Build/OpenWrt-2305-custom/01-uninstall-pkg.sh
+++ Build/OpenWrt-2305-custom/01-uninstall-pkg.sh
@@ -4,3 +4,4 @@
 # uninstall unsupported packages
 
 ./scripts/feeds uninstall nat6-scripts # will not work with new firewall
+./scripts/feeds uninstall nodogsplash # as for jul-2023 leads to package/config conflict with opendns, may be fixed in future
