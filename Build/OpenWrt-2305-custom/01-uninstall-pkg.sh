f3c71923efd7d51ddabbaee8a86c0e824417b89e
DarkCaster <dark.caster@outlook.com>
2023-10-19 03:06:19 +0300

    Build/OpenWrt-2305-custom scripts updated

--- Build/OpenWrt-2305-custom/01-uninstall-pkg.sh
+++ Build/OpenWrt-2305-custom/01-uninstall-pkg.sh
@@ -5,3 +5,4 @@
 
 ./scripts/feeds uninstall nat6-scripts # will not work with new firewall
 ./scripts/feeds uninstall nodogsplash # as for jul-2023 leads to package/config conflict with opendns, may be fixed in future
+./scripts/feeds uninstall pymysql # as for oct-2023 leads to recursive dependency: symbol PACKAGE_python3-pymysql depends on PYTHON3_PYMYSQL_SHA_PASSWORD_SUPPORT
