5a9a94f304429434145c0922c194a09747a74abe
DarkCaster <dark.caster@outlook.com>
2024-01-16 17:37:09 +0300

    Build/OpenWrt-2305-custom scripts updated, switch package feeds from git.openwrt.org to github.com

--- Build/OpenWrt-2305-custom/00-prepare-src.sh
+++ Build/OpenWrt-2305-custom/00-prepare-src.sh
@@ -9,6 +9,9 @@ script_dir="$( cd "$( dirname "$0" )" && pwd )"
 cat feeds.conf.default > feeds.conf
 echo "src-git custom file://${script_dir}/../../../Custom-Feed" >> feeds.conf
 
+sed -i -e 's|git.openwrt.org/feed|github.com/openwrt|g' feeds.conf
+sed -i -e 's|git.openwrt.org/project/luci.git|github.com/openwrt/luci.git|g' feeds.conf
+
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 
