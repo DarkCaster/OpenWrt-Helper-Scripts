1a66a21bc7bdcf548fd03939ae06152703bdbe80
DarkCaster <dark.caster@outlook.com>
2025-07-25 00:04:25 +0300

    Build/OpenWrt-2305-custom: 01-uninstall-pkg.sh updated

--- Build/OpenWrt-2305-custom/01-uninstall-pkg.sh
+++ Build/OpenWrt-2305-custom/01-uninstall-pkg.sh
@@ -3,7 +3,6 @@
 
 # uninstall unsupported packages
 
-./scripts/feeds uninstall nat6-scripts # will not work with new firewall
 ./scripts/feeds uninstall nodogsplash # as for jul-2023 leads to package/config conflict with opendns, may be fixed in future
 ./scripts/feeds uninstall pymysql # as for oct-2023 leads to recursive dependency: symbol PACKAGE_python3-pymysql depends on PYTHON3_PYMYSQL_SHA_PASSWORD_SUPPORT
 ./scripts/feeds uninstall wavemon # as for 2-nov-2023 and v9.5.0 leads to missing dependency warning: libnl-cli
