d03ca98b73d9c6698b7e195a08c3b6ff1a317b92
DarkCaster <dark.caster@outlook.com>
2025-07-24 21:03:08 +0300

    Build/OpenWrt-2305-custom: 00-prepare-src.sh - new method for adding custom repos

--- Build/OpenWrt-2305-custom/00-prepare-src.sh
+++ Build/OpenWrt-2305-custom/00-prepare-src.sh
@@ -7,7 +7,13 @@ script_dir="$( cd "$( dirname "$0" )" && pwd )"
 
 # prepare custom feeds
 cat feeds.conf.default > feeds.conf
-echo "src-git custom file://${script_dir}/../../../Custom-Feed" >> feeds.conf
+echo "src-cpy custom ${script_dir}/../../../Custom-Feed" >> feeds.conf
+
+while IFS= read -r line; do
+  [[ -z $line ]] && continue
+  echo "adding repo $line"
+  echo "src-cpy $line ${script_dir}/../../../Custom-Feed/$line" >> feeds.conf
+done < "${script_dir}/../../../Custom-Feed/enabled-repos-2305.txt"
 
 sed -i -e 's|git.openwrt.org/feed|github.com/openwrt|g' feeds.conf
 sed -i -e 's|git.openwrt.org/project/luci.git|github.com/openwrt/luci.git|g' feeds.conf
